4758b726307757f589636e7f3d1192787758ccc1
student <akpathan560@gmail.com>
2023-07-17 10:07:22 +0530

    Initial commit

---
+++ src/main/kotlin/pr1.1_087.kt
@@ -0,0 +1,23 @@
+fun main() {
+    val a: Int = 6
+    val b: Double = 5.90
+    val c: Char = 'D'
+    val d: Boolean = true
+    val e: String = "Hello"
+    val f: Byte = 100
+    val g: Short = 500
+    val h: Long =100000000L
+    val i: Float = 5.26F
+
+
+    println("Integer value: " +a)
+    println("Double value: " + b)
+    println("char value: "+c)
+    println("boolean value: " +d)
+    println("string value:  " +e)
+    println("byte value: " +f)
+    println("shoet value: " +g)
+    println("Long value: " +h)
+    println("Float value: " +i)
+}
+
