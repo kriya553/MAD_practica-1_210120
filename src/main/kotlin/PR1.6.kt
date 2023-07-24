57b21a09990d535fbb3afa0a74f6ab96486b754f
student <rastogi.dhruv@gmail.com>
2023-07-24 10:45:43 +0530

    hi

---
+++ src/main/kotlin/PR1.6.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    val number1 = 12.5
+    val number2 = 3.5
+
+    var result: Double = number1 + number2
+    println("number1 + number2 = $result")
+
+    result = number1 - number2
+    println("number1 - number2 = $result")
+
+    result = number1 * number2
+    println("number1 * number2 = $result")
+
+    result = number1 / number2
+    println("number1 / number2 = $result")
+
+    result = number1 % number2
+    println("number1 % number2 = $result")
+}
---
+++ src/main/kotlin/pr1.4.kt
@@ -0,0 +1,21 @@
+import java.util.*
+
+fun main() {
+
+    val reader = Scanner(System.`in`)
+
+    print("Enter a number: ")
+    val num = reader.nextInt()
+
+    if (num % 2 == 0)
+        println("$num is even")
+    else
+        println("$num is odd")
+}
+
+
+
+
+
+
+
